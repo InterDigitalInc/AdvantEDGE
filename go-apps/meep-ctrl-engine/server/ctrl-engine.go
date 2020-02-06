1ec47e7ea98722619de188b7c87ac7a478e6bf94
Kevin Di Lallo <kevin.dilallo@interdigital.com>
2020-02-05 19:59:53 -0500

    patch to ignore mobility events with no poa change

--- go-apps/meep-ctrl-engine/server/ctrl-engine.go
+++ go-apps/meep-ctrl-engine/server/ctrl-engine.go
@@ -610,7 +610,7 @@ func ceSendEvent(w http.ResponseWriter, r *http.Request) {
 
 	// Log successful event in metric store
 	eventJSONStr, err := json.Marshal(event)
-	if err == nil {
+	if err == nil && description != "" {
 		var metric ms.EventMetric
 		metric.Event = string(eventJSONStr)
 		metric.Description = description
@@ -778,6 +778,9 @@ func sendEventMobility(event ceModel.Event) (error, int, string) {
 		"meep.log.dest":      elemName,
 	}).Info("Measurements log")
 
+	if newNL == oldNL {
+		description = ""
+	}
 	return nil, -1, description
 }
 
