9be63830ee7775606a73ae90d0afa8e3aa2125e3
Michel Roy <michel.roy@interdigital.com>
2020-05-01 15:00:31 -0400

    fix lint

--- go-apps/meepctl/cmd/deploy.go
+++ go-apps/meepctl/cmd/deploy.go
@@ -199,7 +199,7 @@ func deployRunScriptsAndGetFlags(targetName string, chart string, cobraCmd *cobr
 	case "meep-couchdb":
 		flags = utils.HelmFlags(nil, "--set", "persistentVolume.location="+deployData.workdir+"/couchdb/")
 	case "meep-open-map-tiles":
-			flags = utils.HelmFlags(nil, "--set", "persistentVolume.location="+deployData.workdir+"/omt/")
+		flags = utils.HelmFlags(nil, "--set", "persistentVolume.location="+deployData.workdir+"/omt/")
 	case "meep-docker-registry":
 		deployCreateRegistryCerts(chart, cobraCmd)
 		flags = utils.HelmFlags(nil, "--set", "persistence.location="+deployData.workdir+"/docker-registry/")
