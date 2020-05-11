8b8ce9ac323b38837460c1d9ed5c653218b85941
Michel Roy <michel.roy@interdigital.com>
2020-05-11 15:46:18 -0400

    fix add  postgis to meepctl

--- go-apps/meepctl/cmd/deploy.go
+++ go-apps/meepctl/cmd/deploy.go
@@ -202,8 +202,8 @@ func deployRunScriptsAndGetFlags(targetName string, chart string, cobraCmd *cobr
 		flags = utils.HelmFlags(nil, "--set", "persistentVolume.location="+deployData.workdir+"/couchdb/")
 	case "meep-open-map-tiles":
 		flags = utils.HelmFlags(nil, "--set", "persistentVolume.location="+deployData.workdir+"/omt/")
-  case "meep-postgis":
-			flags = utils.HelmFlags(nil, "--set", "persistentVolume.location="+deployData.workdir+"/postgis/")
+        case "meep-postgis":
+		flags = utils.HelmFlags(nil, "--set", "persistence.location="+deployData.workdir+"/postgis/")
 	case "meep-docker-registry":
 		deployCreateRegistryCerts(chart, cobraCmd)
 		flags = utils.HelmFlags(nil, "--set", "persistence.location="+deployData.workdir+"/docker-registry/")
