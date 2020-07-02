106379881ec761c25e4778d8c2cbe907d860485e
Kevin Di Lallo <kevin.dilallo@interdigital.com>
2020-07-02 10:41:00 -0400

    meepctl fix when no config file present

--- go-apps/meepctl/utils/config.go
+++ go-apps/meepctl/utils/config.go
@@ -357,11 +357,13 @@ func ConfigIsIpv4(host string) bool {
 //  operation == "" returns the whole group
 func GetTargets(group string, operation string) []string {
 	targets := []string{}
-	for target := range RepoCfg.GetStringMapString(group) {
-		if RepoCfg.GetBool(group+"."+target+"."+operation) || operation == "" {
-			targets = append(targets, target)
+	if RepoCfg != nil {
+		for target := range RepoCfg.GetStringMapString(group) {
+			if RepoCfg.GetBool(group+"."+target+"."+operation) || operation == "" {
+				targets = append(targets, target)
+			}
 		}
+		sort.Strings(targets)
 	}
-	sort.Strings(targets)
 	return targets
 }
